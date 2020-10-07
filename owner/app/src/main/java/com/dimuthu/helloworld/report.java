529fbb9d471bf1a1c880e63557fcfbfaf4578eef
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-10-08 00:14:01 +0530

    owner interface

---
+++ owner/app/src/main/java/com/dimuthu/helloworld/report.java
@@ -0,0 +1,14 @@
+package com.dimuthu.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
