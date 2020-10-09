507e0666fe1caad3e8a6fceb950fb9aa89658796
randiepathirage <randiepathirage@gmail.com>
2020-10-09 12:51:24 +0530

    navigation bar added

--- parent/app/src/main/java/com/e/parent/MainActivity.java
+++ parent/app/src/main/java/com/e/parent/MainActivity.java
@@ -2,15 +2,29 @@ package com.e.parent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("easyVan");
+        getSupportActionBar().setTitle("Home");
+
+        button=(Button)findViewById(R.id.btnNewsFeed);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),newsfeedfrag.class);
+                startActivity(intent);
+            }
+        });
     }
 }
