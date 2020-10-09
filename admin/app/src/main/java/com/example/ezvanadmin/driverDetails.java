eac62b2ee0e13049af0944d974c4badf2f6faa9e
Ruvidu <2018cs106@stu.ucsc.cmb.ac.lk>
2020-10-10 02:20:04 +0530

    admin interfaces

---
+++ admin/app/src/main/java/com/example/ezvanadmin/driverDetails.java
@@ -0,0 +1,14 @@
+package com.example.ezvanadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class driverDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_details);
+    }
+}
---
+++ admin/app/src/main/java/com/example/ezvanadmin/newArticle.java
@@ -0,0 +1,14 @@
+package com.example.ezvanadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_article);
+    }
+}
