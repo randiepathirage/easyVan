c8686f5cf84b3b61162d63c0abe652456f43baac
DeemanthaAbeywickrama <deemanthaabeywickrama96@gmail.com>
2020-10-11 13:59:21 +0530

    child details

--- Driver/app/src/main/java/com/example/driver/ChildDetails.java
+++ Driver/app/src/main/java/com/example/driver/ChildDetails.java
@@ -2,13 +2,27 @@ package com.example.driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildDetails extends AppCompatActivity {
+
+    Button ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_details);
+
+        ok = findViewById(R.id.btnok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ViewChildDetails.class);
+                startActivity(i);
+            }
+        });
     }
 }
