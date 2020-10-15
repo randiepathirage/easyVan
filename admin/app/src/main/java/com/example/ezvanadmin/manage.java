00f2d979a0b2d54c17ce7a11c2a6f73676d2378e
Ruvidu <2018cs106@stu.ucsc.cmb.ac.lk>
2020-10-15 22:36:07 +0530

    zzz

--- admin/app/src/main/java/com/example/ezvanadmin/manage.java
+++ admin/app/src/main/java/com/example/ezvanadmin/manage.java
@@ -93,4 +93,19 @@ public class manage extends AppCompatActivity {
             }
         });
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.app_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.notification:
+                startActivity(new Intent(this,notification.class));
+                return true;
+
+        }
+        return true;
+    }
 }
