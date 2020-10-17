15a58002774d9e8ade4d50dd9714544d05b0c5b0
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-10-17 22:07:39 +0530

    manage modified

--- app/easyVan/app/src/main/java/com/e/esayVan/OwnerManage.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/OwnerManage.java
@@ -21,6 +21,28 @@ public class OwnerManage extends AppCompatActivity {
 
     private Menu menu;
     Button driverBtn, vanBtn;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.owner_appbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.notification:
+                startActivity(new Intent(this,OwnerNotification.class));
+                return true;
+
+            case R.id.reqest:
+                startActivity(new Intent(this,OwnerRequest.class));
+                return true;
+
+            case R.id.account:
+                startActivity(new Intent(this,OwnerAccount.class));
+                return true;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
