393736526375db1f787e63fa5f6f229e9be0c27d
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2020-10-21 19:44:36 +0530

    Main UI

--- app/easyVan/app/src/main/java/com/e/esayVan/Owner.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/Owner.java
@@ -75,47 +75,7 @@ public class Owner extends AppCompatActivity {
             }
         });
 
-        /*declar variable*/
-        BottomNavigationView bottomNavigationView ;
-
-        /*selected */
-        bottomNavigationView=findViewById(R.id.bottom_navigation);
-        /*Set manage*/
-        bottomNavigationView.setSelectedItemId(R.id.manage);
-        /*performe*/
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.manage:
-                        return true;
-
-                    case R.id.expenses:
-                        Intent j = new Intent(getApplicationContext(),OwnerExpenses.class);
-                        startActivity(j);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.report:
-                        Intent l = new Intent(getApplicationContext(),OwnerReport.class);
-                        startActivity(l);
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.calender:
-                        Intent k = new Intent(getApplicationContext(),OwnerCalendar.class);
-                        startActivity(k);
-                        overridePendingTransition(0,0);
-                        return true;
-
-
-
-                    case R.id.location:
-                        Intent m = new Intent(getApplicationContext(),OwnerLocation.class);
-                        startActivity(m);
-                        overridePendingTransition(0,0);
-                        return true;
-                }
+  
 
                 return false;
             }
