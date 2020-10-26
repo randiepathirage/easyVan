3a8534e5041f91eed01d979b1b0a88f8f93f07a7
Ruvidu <2018cs106@stu.ucsc.cmb.ac.lk>
2020-10-26 20:42:20 +0530

    zz

--- app/easyVan/app/src/main/java/com/e/esayVan/AdminParents.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/AdminParents.java
@@ -20,7 +20,7 @@ public class AdminParents extends AppCompatActivity {
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent addIntent = new Intent(AdminParents.this,AdminAddNewParent.class);
+                Intent addIntent = new Intent(AdminParents.this,AddNewUser.class);
                 startActivity(addIntent);
             }
         });
