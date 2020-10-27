ecdbda703e4f48f082b98655da37ecd94f970506
randiepathirage <randiepathirage@gmail.com>
2020-10-27 10:19:52 +0530

    registeration

--- app/easyVan/app/src/main/java/com/e/esayVan/AdminAddNewParent.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/AdminAddNewParent.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class sample extends AppCompatActivity {
+public class AdminAddNewParent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample);
+        setContentView(R.layout.activity_admin_add_new_parent);
+        getSupportActionBar().setTitle("Add New Parent");
     }
 }
