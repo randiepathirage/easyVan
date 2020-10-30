c2f0c7d6fb5c95f6dbbfd1f02049259ad8c60e58
Ruvidu <56601539+Ruvidu@users.noreply.github.com>
2020-10-30 14:18:39 +0530

    Update SignUp.java

--- app/easyVan/app/src/main/java/com/e/esayVan/SignUp.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/SignUp.java
@@ -1,20 +1,18 @@
 package com.e.esayVan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
     EditText firstName,middleName,lastName,NICNo,username,password,address,contactNo,email,confirmPassword;
     RadioGroup radioGroup;
     RadioButton radioParent,radioOwner;
-    AwesomeValidation awesomeValidation;
+    //AwesomeValidation awesomeValidation;
 
 
     @Override
@@ -24,7 +22,6 @@ public class SignUp extends AppCompatActivity {
         getSupportActionBar().setTitle("Sign Up");
 
         firstName=(EditText)findViewById(R.id.edtFirstname);
-        middleName=(EditText)findViewById(R.id.edtMiddlename);
         lastName=(EditText)findViewById(R.id.edtLastname);
         NICNo=(EditText)findViewById(R.id.edtNic);
         username=(EditText)findViewById(R.id.edtUsername);
@@ -41,23 +38,180 @@ public class SignUp extends AppCompatActivity {
 
         //validation
        /* awesomeValidation=new AwesomeValidation(ValidationStyle.BASIC);
-
         awesomeValidation.addValidation(this,R.id.edtFirstname,
                 RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-
         awesomeValidation.addValidation(this,R.id.edtMiddlename,
                 RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-
         awesomeValidation.addValidation(this,R.id.edtLastname,
                 RegexTemplate.NOT_EMPTY,R.string.invalid_name);
-
         awesomeValidation.addValidation(this,R.id.edtPassword,
                 ".{6,}",R.string.invalid_password);
-
         awesomeValidation.addValidation(this,R.id.edtConfirmPassword,
                 R.id.edtPassword,R.string.invalid_confirm_password);*/
-}
+    }
+
+    private Boolean validateName(){
+        String val_firstName= firstName.getText().toString();
+        String val_lastName= lastName.getText().toString();
+
+        if(val_firstName.isEmpty() & val_lastName.isEmpty()){
+            firstName.setError("This field cannot be empty");
+            lastName.setError("This field cannot be empty");
+            return false;
+        }
+        else if(val_lastName.isEmpty()){
+            lastName.setError("This field cannot be empty");
+            firstName.setError(null);
+            return false;
+        }
+        else if(val_firstName.isEmpty()){
+            firstName.setError("This field cannot be empty");
+            lastName.setError(null);
+            return false;
+        }
+        else{
+            firstName.setError(null);
+            lastName.setError(null);
+            return true;
+        }
+    }
+
+  /*  private Boolean validateNIC(){
+        String str_NICNo= NICNo.getText().toString();
+        if(val_firstName.isEmpty() & val_lastName.isEmpty()){
+            firstName.setError("This field cannot be empty");
+            lastName.setError("This field cannot be empty");
+            return false;
+        }
+        else if(val_lastName.isEmpty()){
+            lastName.setError("This field cannot be empty");
+            firstName.setError(null);
+            return false;
+        }
+        else if(val_firstName.isEmpty()){
+            firstName.setError("This field cannot be empty");
+            lastName.setError(null);
+            return false;
+        }
+        else{
+            firstName.setError(null);
+            lastName.setError(null);
+            return true;
+        }
+    }*/
+
+    private Boolean validateUsername(){
+        String val_username= username.getText().toString();
+        String noWhitespaces="\\A\\w{4,20}\\z";
+
+        if(val_username.isEmpty() ){
+            username.setError("This field cannot be empty");
+            return false;
+        }
+        else if (!val_username.matches(noWhitespaces)) {
+            username.setError("White Spaces are not allowed");
+            return false;
+        }
+        else{
+            username.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail(){
+        String val_email=email.getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(val_email.isEmpty()){
+            email.setError("This field cannot be empty");
+            return false;
+        }
+        else if(!val_email.matches(emailPattern)){
+            email.setError("Invalid email address");
+            return false;
+        }
+        else{
+            email.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateContactNo(){
+        String val_contactNo= contactNo.getText().toString();
+
+        if(val_contactNo.isEmpty()){
+            contactNo.setError("This field cannot be empty");
+            return false;
+        }
+        else{
+            contactNo.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword(){
+        String val_password= password.getText().toString();
+        String passwordVal = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                "(?=.*[a-z])" +         //at least 1 lower case letter
+                "(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if(val_password.isEmpty()){
+            password.setError("This field cannot be empty");
+            return false;
+        }
+        else if(!val_password.matches(passwordVal)){
+            password.setError("Password is too week");
+            return false;
+        }
+        else{
+            password.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateConfirmPassword(){
+        String val_confirmPassword= confirmPassword.getText().toString();
+        String val_password= password.getText().toString();
+
+        if(val_confirmPassword.isEmpty()){
+            confirmPassword.setError("This field cannot be empty");
+            return false;
+        }
+        else if(val_confirmPassword!=val_password){
+            confirmPassword.setError("Password doesn't match");
+            return false;
+        }
+        else{
+            confirmPassword.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateAddress(){
+        String val_address= address.getText().toString();
+
+        if(val_address.isEmpty()){
+            address.setError("This field cannot be empty");
+            return false;
+        }
+        else{
+            address.setError(null);
+            return true;
+        }
+    }
+
     public void onReg(View view){
+
+        if(!validateName()|!validateUsername()|!validatePassword()
+                |!validateContactNo()|!validateEmail()|!validateConfirmPassword()|!validateAddress()){
+            return;
+        }
 
         String user_role;
         if(radioParent.isChecked()){
@@ -66,9 +220,7 @@ public class SignUp extends AppCompatActivity {
             user_role="owner";
         }
 
-
-        String Str_firstName= username.getText().toString();
-        String str_middleName= middleName.getText().toString();
+        String Str_firstName= firstName.getText().toString();
         String str_lastName= lastName.getText().toString();
         String str_NICNo= NICNo.getText().toString();
         String str_username= username.getText().toString();
@@ -80,7 +232,7 @@ public class SignUp extends AppCompatActivity {
         String type = "register";
 
         BackgroundWorker backgroundWorker=new BackgroundWorker(this);
-        backgroundWorker.execute(type,Str_firstName,str_middleName,
+        backgroundWorker.execute(type,Str_firstName,
                 str_lastName,str_NICNo,str_username,str_password,str_address,
                 str_contactNo,str_email,user_role);
     }
