06971a7e9cdac1f51c22d7c7edc5e1f11b4d2c9b
randiepathirage <randiepathirage@gmail.com>
2020-11-11 18:34:17 +0530

    Update viewParentDetails.php

--- app/database/parent/viewParentDetails.php
+++ app/database/parent/viewParentDetails.php
@@ -4,13 +4,17 @@
     //randie...............................................................
     require  "conn.php";
 
-    //$user_name=$_POST["username"];
-    $user_name="randie";
+    $user_name=$_POST["username"];
+    //$user_name="randie";
 
     $user = array();
 
     //access user table
-    $stmt = "SELECT NIC_no , contact_no , last_name , first_name , address  FROM user WHERE NIC_no IN(SELECT NIC_no FROM login WHERE username='$user_name')";//creating a query
+    //$stmt = "SELECT user.NIC_no , user.contact_no , user.address,login.email  FROM user INNER JOIN login ON user.NIC_no= login.NIC_no WHERE login.username='$user_name')";//creating a query
+    $stmt = "SELECT user.NIC_no,user.contact_no,user.address,login.email FROM `user` INNER JOIN login ON user.NIC_no=login.NIC_no WHERE login.username='$user_name'";//creating a query
+
+    //SELECT first_name,last_name FROM user INNER JOIN parent_owner_driver ON parent_owner_driver.NIC_no = user.NIC_no where owner_flag=1";
+
     $stmt_exe=mysqli_query($conn,$stmt);//executing the query
 
    $result=array();
@@ -21,9 +25,8 @@
 
         $index['NIC_no']= $row['NIC_no'];
         $index['contact_no']= $row['contact_no'];
-        $index['last_name']= $row['last_name'];
-        $index['first_name']= $row['first_name'];
         $index['address'] = $row['address'];
+        $index['email'] = $row['email'];
 
       /*  $temp = array();
         $temp['NIC_no'] = $NIC_no; 
