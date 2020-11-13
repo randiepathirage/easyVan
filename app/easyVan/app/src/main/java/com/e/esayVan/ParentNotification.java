77ce28d03404894578b07f67bb1f74599d4f290b
randiepathirage <randiepathirage@gmail.com>
2020-11-13 15:03:22 +0530

    driver app bar edited

--- app/easyVan/app/src/main/java/com/e/esayVan/ParentNotification.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/ParentNotification.java
@@ -9,7 +9,7 @@ public class ParentNotification extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+        setContentView(R.layout.activity_parent_notification);
         getSupportActionBar().setTitle("Notifications");
     }
 }
