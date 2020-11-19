b9ccd4bee65adf5eaae18836fb90158c26aa699e
DeemanthaAbeywickrama <56666885+DeemanthaAbeywickrama@users.noreply.github.com>
2020-11-19 16:01:13 +0530

    driver new view

--- app/easyVan/app/src/main/java/com/e/esayVan/DriverChildDetails.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/DriverChildDetails.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +27,7 @@ public class DriverChildDetails extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),DriverViewChildDetails.class);
+                Intent i = new Intent(DriverChildDetails.this,DriverViewChildDetails.class);
                 startActivity(i);
             }
         });
