1798ff8a4f6a1c57039c373dd1ab471e92a08882
DeemanthaAbeywickrama <56666885+DeemanthaAbeywickrama@users.noreply.github.com>
2020-11-23 13:24:45 +0530

    driver top appbar change

--- app/easyVan/app/src/main/java/com/e/esayVan/DriverCalendar.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/DriverCalendar.java
@@ -61,4 +61,41 @@ public class DriverCalendar extends AppCompatActivity {
             }
         });
     }
+
+    private Menu menu;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.driver_appbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.top_notification:
+                Intent m = new Intent(getApplicationContext(),DriverNotification.class);
+                startActivity(m);
+                return true;
+            case R.id.top_profile:
+                Intent i = new Intent(getApplicationContext(),DriverProfile.class);
+                startActivity(i);
+                return true;
+            case R.id.top_calendar:
+                return true;
+            case R.id.top_payment:
+                Intent j = new Intent(getApplicationContext(),DriverPayment.class);
+                startActivity(j);
+                return true;
+
+
+            case R.id.logout:
+                SessionManagement sessionManagement = new SessionManagement(DriverCalendar.this);
+                sessionManagement.removeSession();
+
+                Intent intent = new Intent(DriverCalendar.this, Login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+        }
+        return true;
+    }
 }
