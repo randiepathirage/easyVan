3649f0ea55070600b7260da04a38188cff243ce0
randiepathirage <52100121+randiepathirage@users.noreply.github.com>
2021-03-16 20:46:50 +0530

    Update adduser.php

--- app/database/adduser.php
+++ app/database/adduser.php
@@ -1,6 +1,13 @@
 <?php
     require  "conn.php";
+
+    $file = fopen('ApplicationLogs.txt','a');
+
+    date_default_timezone_set("Asia/Colombo");
+    $day=date("Y-m-d");
+    $time=date("h:i:sa");
     
+    /*
     $license_no=0;
     $user_firstname=$_POST['firstName'];
     $user_lastname=$_POST['lastName'];
@@ -10,22 +17,38 @@
     $address=$_POST['address'];
     $contact_no=$_POST['contactNo'];
     $email=$_POST['email'];
-    $user_role=$_POST['userRole'];
+    $user_role=$_POST['userRole'];*/
 
     
-   /* 
+   
     $user_firstname="A";
     $user_lastname="A";
-    $nic_no="98765ere5v";
-    $username="reeresasdfvb";
+    $nic_no="33765ere5v";
+    $username="resasdfvb";
     $password="123";
     $address="A";
-    $contact_no=55523165456;
-    $email="rfgvvffsvvcd";
+    $contact_no=432265456;
+    $email="rfsvvcd";
     $user_role="owner";
-    $license_no=0;*/
+    $license_no=0;
 
-    $password=md5($password);
+    //$password=md5($password);
+
+     $salt=generateRandomString();
+
+    function generateRandomString($length = 20) {
+       $characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|[]/?~`';
+       $charactersLength = strlen($characters);
+       $randomString = '';
+       for ($i = 0; $i < $length; $i++) {
+           $randomString .= $characters[rand(0, $charactersLength - 1)];
+       }
+       return $randomString;
+    }
+
+    //$password=md5($password);
+    $password=$password.$salt;
+    $password=hash('sha256',$password);
     
     $parent=0;
     $driver=0;
@@ -58,20 +81,24 @@
 
 
     if(mysqli_num_rows($email_result)>0){
-        echo "This email is already registered";  
+        echo "This email is already registered";
+        fwrite($file,"\n$day  $time   $nic_no      REGISTRATION fail      ERROR");  
     }
     else if(mysqli_num_rows($username_result)>0){
-        echo "This username is already taken"; 
+        echo "This username is already taken";
+        fwrite($file,"\n$day  $time   $nic_no      REGISTRATION fail      ERROR"); 
     }
     else if(mysqli_num_rows($nic_result)>0){
-        echo "This NIC number is already registered"; 
+        echo "This NIC number is already registered";
+        fwrite($file,"\n$day  $time   $nic_no      REGISTRATION fail      ERROR"); 
     }
 
     else if(mysqli_num_rows($contact_result)>0){
-        echo "This contact number is already registered"; 
+        echo "This contact number is already registered";
+        fwrite($file,"\n$day  $time   $nic_no      REGISTRATION fail      ERROR"); 
     }
     else{
-        $query_login="INSERT INTO login(NIC_no,username,password,email) VALUES ('$nic_no','$username','$password','$email')";
+        $query_login="INSERT INTO login(NIC_no,username,password,salt,email) VALUES ('$nic_no','$username','$password','$salt','$email')";
 
         $query_user="INSERT INTO user(NIC_no,contact_no,last_name,first_name,address) VALUES ('$nic_no','$contact_no','$user_lastname','$user_firstname','$address')";
 
@@ -79,12 +106,15 @@
 
         if($conn->query($query_login)===TRUE && $conn->query($query_user)===TRUE && $conn->query($query_parent_owner_driver)===TRUE){
             echo "User Added Successfully";
+            fwrite($file,"\n$day  $time   $nic_no      REGISTRATION $user_role    ALERT");
                 
         }
         else{
             echo "Error".$query_login."<br>".$conn->error;
+            fwrite($file,"\n$day  $time   $nic_no      REGISTRATION fail       ERROR");
         }
     }
         $conn->close();
+        fclose($file);
 
 ?>
