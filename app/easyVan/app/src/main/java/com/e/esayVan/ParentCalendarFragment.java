22ff962946429f8fc0719245fb0863395183079c
randiepathirage <52100121+randiepathirage@users.noreply.github.com>
2021-03-19 11:53:02 +0530

    parent mark attandance interface completed

--- app/easyVan/app/src/main/java/com/e/esayVan/ParentCalendarFragment.java
+++ app/easyVan/app/src/main/java/com/e/esayVan/ParentCalendarFragment.java
@@ -11,21 +11,47 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ParentCalendarFragment extends AppCompatActivity {
 
     BottomNavigationView bottom_nav;
     Button btnMarkAttend;
     EditText edtDate;
-    private Spinner spinner;
+    String strNic="",vehicleNo="",ownerID="",userName;
 
-    DatePickerDialog.OnDateSetListener setListener;
+    String VIEW_CHILD_URL="http://10.0.2.2/easyvan/viewChildDetails.php";
+
+    //a list to store all the child details
+    List<ParentChild> childlist;
+
+    //the recyclerview
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +82,23 @@ public class ParentCalendarFragment extends AppCompatActivity {
             }
         });
 
-        //add spinner to select child
-        spinner=findViewById(R.id.spinner);
-      //  ArrayAdapter<String> adapter=new ArrayAdapter<String>(ParentCalendarFragment.this,
-     //           android.R.layout.simple_list_item_1,getResources().getStringArray(R.strings.names));
-     //   adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     //   adapter.setAdaptor(adapter);
+        //view children details
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView.ItemDecoration divider=new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(divider);
+
+        //initializing the childrenlist
+        childlist = new ArrayList<>();
+
+        //get the session username
+        SessionManagement sessionManagement = new SessionManagement(this);
+        userName = sessionManagement.getUserName();
+
+        loadChildList();
 
         btnMarkAttend=findViewById(R.id.btnMarkAttend);
         btnMarkAttend.setOnClickListener(new View.OnClickListener() {
@@ -109,6 +146,62 @@ public class ParentCalendarFragment extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void loadChildList() {
+
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST,VIEW_CHILD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray array=new JSONArray(response);
+
+                            for(int i=0;i<array.length();i++){
+                                JSONObject child=array.getJSONObject(i);
+                                childlist.add(new ParentChild(
+                                        child.getString("grade"),
+                                        child.getString("school"),
+                                        child.getString("firstName"),
+                                        child.getString("lastName"),
+                                        child.getString("pickupLocation"),
+                                        child.getString("dropoffLocation"),
+                                        child.getString("childNo")
+
+                                ));
+
+                            }
+
+                            //creating recyclerview adapter
+                            ParentChildrenList adapter = new ParentChildrenList(ParentCalendarFragment.this,childlist);
+                            //setting adapter to recyclerview
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ParentCalendarFragment.this,error.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String,String>();
+
+                params.put("username",userName);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 
     //app bar
