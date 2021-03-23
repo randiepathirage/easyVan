e6624b40adea209c256c9aca9c3e7056805c67fa
unknown <ruvidu>
2021-03-23 18:53:32 +0530

    switch account

--- app/database/admin/updatechild.php
+++ app/database/admin/updatechild.php
@@ -10,7 +10,7 @@
   $grade=$_POST['grade'];
   $pickup=$_POST['pickup_location'];
   $vehicle_no=$_POST['vehicle_no'];
-  $fee=$_POST['monthly_fee'];
+  $fees=$_POST['fees'];
   $pname=$_POST['parent_name'];
 
     /*$cnum="2";
@@ -25,7 +25,7 @@
     $pname="zz";*/
 
 
-$Sql_Query_child = "UPDATE child SET first_name = '$firstname', last_name = '$lastname', grade = '$grade', school = '$school', pickup_location = '$pickup', vehicle_no = '$vehicle_no', monthly_fee = '$fee' WHERE child_no = '$cnum'";
+$Sql_Query_child = "UPDATE child SET first_name = '$firstname', last_name = '$lastname', grade = '$grade', school = '$school', pickup_location = '$pickup', vehicle_no = '$vehicle_no', fees = '$fees' WHERE child_no = '$cnum'";
 
  if($conn->query($Sql_Query_child)===TRUE){
             echo 'Record Updated Successfully';
